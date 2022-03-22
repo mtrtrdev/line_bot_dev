fe9271b4e70815400812c7888d7e2f82c2b265c0
mtrtrdev <mtrtrdev@gmail.com>
2022-03-22 17:24:12 +0900

    4th commit

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def callback():
 def handle_message(event):
     text=event.message.text #検索文字列
     lists=scrape.getNews(text) #スクレイピング
-    if not lists:
+    if any(lists) == False:
         line_bot_api.reply_message(
     event.reply_token, TextSendMessage("検索結果がありません。終了します。"))
         return
